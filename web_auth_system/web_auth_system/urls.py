5366267f647473d6d83acea5484758b8595296f7
Antunes Dos Santos <leonardo-antunes-dos-santos@siemens.com>
2024-09-23 17:44:51 -0300

    change project name

--- web_auth_system/web_auth_system/urls.py
+++ web_auth_system/web_auth_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for finance_platform project.
+URL configuration for web_auth_system project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
